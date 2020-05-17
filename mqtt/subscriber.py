26be3ec2a72b64a58cd463259fb8076cea3f9def
NgurahWira09 <64675410+NgurahWira09@users.noreply.github.com>
2020-05-17 21:59:27 +0800

    Done subsriber

--- mqtt/subscriber.py
+++ mqtt/subscriber.py
@@ -1,10 +1,8 @@
-"""This script is an example of subscriber to CloudMQTT Broker"""
-
 from urllib.parse import urlparse
 import os
 import paho.mqtt.client as mqtt
+import time
 
-# Define event callbacks
 def on_connect(mosq, obj, rc):
     print('Reconnect: {}'.format(str(rc)))
 
@@ -18,35 +16,37 @@ def on_subscribe(mosq, obj, mid, granted_qos):
 def on_log(mosq, obj, level, string):
     print(string)
 
-username = 'your username'
-password = 'your password'
-host_url = 'your cloud broker url'
-host_port = 'your cloud broker port' # do not type string, but change to integer
 
-mqttc = mqtt.Mosquitto()
+# username = 'your username'
+# password = 'your password'
+host_url = 'mqtt.eclipse.org'
+host_port = 1883  # do not type string, but change to integer
+
+mqttc = mqtt.Client()
 # Assign event callbacks
 mqttc.on_message = on_message
-mqttc.on_connect = on_connect
+mqttc.connect('mqtt.eclipse.org', 1883)
 mqttc.on_subscribe = on_subscribe
 
 # Uncomment to enable debug messages
 #mqttc.on_log = on_log
 
 # Parse CLOUDMQTT_URL (or fallback to localhost)
-url_str = os.environ.get('CLOUDMQTT_URL', host_url)
-url = urlparse(url_str)
+# url_str = os.environ.get('CLOUDMQTT_URL', host_url)
+# url = urlparse(url_str)
 
 # Connect
-username = 'your username'
-password = 'your password'
-mqttc.username_pw_set(username, password)
-mqttc.connect(url.hostname, url.port)
+# username = 'your username'
+# password = 'your password'
+# mqttc.username_pw_set(username, password)
+# mqttc.connect(url.hostname, url.port)
 
 # Subscribe to a topic
-mqttc.subscribe('python/', 0)
+mqttc.subscribe('python/+/socket')
 
 # Continue the network loop, exit when an error occurs
 rc = 0
 while rc == 0:
     rc = mqttc.loop()
+    time.sleep(2)
 print('Reconnect: ' + str(rc))
